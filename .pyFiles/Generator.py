0f5b4059cf71779354fc506acddde8321a5fa1c9
Shaqayeq Matlabi <108318987+shgyg99@users.noreply.github.com>
2024-09-14 08:51:53 +0330

    moved .pt files into .ptFiles folder

--- .pyFiles/Generator.py
+++ .pyFiles/Generator.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 import torch
 
+model = torch.load('/content/drive/MyDrive/all_data/model.pt', map_location=device)
+model.eval()
+test_loader = torch.load('/content/drive/MyDrive/all_data/test.pt')
+vocab = torch.load('/content/drive/MyDrive/all_data/vocab.pt')
+loss_fn = nn.CrossEntropyLoss(ignore_index=vocab['<pad>'])
+
 x, y = next(iter(test_loader))
 idx = torch.randint(0, x.shape[0], (1,))
 text = generate(x[[idx], ...], model, vocab, max_seq_length=20, device=device)
